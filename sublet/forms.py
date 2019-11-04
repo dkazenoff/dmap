69636b4221ed8fc4ef44ddebef4026f291aadc2d
Dan K <dkazenoff@gmail.com>
2019-11-04 16:43:19 -0500

    Misc. Commenting

--- sublet/forms.py
+++ sublet/forms.py
@@ -1,15 +1,17 @@
 from sublet.models import CASUser,Listing
 from django import forms
 
+# Create's the user / schema
 class NewUserForm(forms.ModelForm):
 	class Meta:
 		model = CASUser
 		fields = ['first_name','last_name']
+# The attributes we want rendered on the listing form
 
 class ListingForm(forms.ModelForm):
 	class Meta:
 		model = Listing
-		exclude = ['owner']
+		exclude = ['owner']	# Filter, since this is already passed in
 		widgets = {
             'address': forms.TextInput(attrs={'placeholder': 'Sublet address', 'class': 'form-control form-control-lg'}),
             'rent': forms.TextInput(attrs={'placeholder': 'Rent per month', 'class': 'form-control form-control-lg','step':'0.01'}),
