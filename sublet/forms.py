fc3c4089fe76a05919e0c1a238bc082a16fa60e6
Dan <dkazenoff@gmail.com>
2019-11-23 23:58:21 -0500

    Quick edits

--- sublet/forms.py
+++ sublet/forms.py
@@ -20,7 +20,7 @@ class ListingForm(forms.ModelForm):
 		exclude = ['owner']	# Filter, since this is already passed in
 		widgets = {
             'address': forms.TextInput(attrs={'placeholder': 'Sublet address', 'class': 'form-control form-control-lg'}),
-            'rent': forms.TextInput(attrs={'placeholder': 'Rent per month', 'class': 'form-control form-control-lg','step':'0.01'}),
+            'rent': forms.TextInput(attrs={'placeholder': 'Rent per month (per room)', 'class': 'form-control form-control-lg','step':'0.01'}),
             'bedrooms': forms.TextInput(attrs={'placeholder': '# of bedrooms', 'class': 'form-control form-control-lg'}),
             'bathrooms': forms.TextInput(attrs={'placeholder': '# of bathrooms', 'class': 'form-control form-control-lg'}),
             'distance': forms.TextInput(attrs={'placeholder': 'Distance from RPI', 'class': 'form-control form-control-lg','step':'0.1'}),
